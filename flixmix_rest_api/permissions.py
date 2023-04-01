c44f63fef949534d4ab9f2ec9b9bcd3c26173337
Agustin cilli <cilli.agustin@gmail.com>
2023-04-01 15:34:34 +0000

    Create defs so only the superuser or the owner has permission

--- flixmix_rest_api/permissions.py
+++ flixmix_rest_api/permissions.py
@@ -11,3 +11,17 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
             return True
         # if user is owner give permissions
         return obj.owner == request.user
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_superuser
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
