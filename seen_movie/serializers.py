d92c34b0a48f7dffa8c8653ec7b43fa8efc09996
Agustin cilli <cilli.agustin@gmail.com>
2023-04-02 12:24:00 +0000

    Add seen serializers

---
+++ seen_movie/serializers.py
@@ -0,0 +1,22 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Seen
+
+
+class SeenSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    movie_title = serializers.ReadOnlyField(source='movie.title')
+
+    class Meta:
+        model = Seen()
+        fields = [
+            'id', 'owner', 'movie', 'movie_title', 'created_at'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate!'
+            })
