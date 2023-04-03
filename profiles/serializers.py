392bb43123994e6b8782a23b503d4dc2a9b8c5ca
Agustin cilli <cilli.agustin@gmail.com>
2023-04-03 08:15:28 +0000

    Add movie, followers and following count to profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,11 +7,14 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    movie_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-    
+
     def get_following_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
@@ -20,12 +23,12 @@ class ProfileSerializer(serializers.ModelSerializer):
             ).first()
             return following.id if following else None
         return None
-            
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'description', 'image', 'favorite_genre', 'is_admin',
-            'is_owner', 'following_id'
+            'is_owner', 'following_id', 'movie_count',
+            'followers_count', 'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from flixmix_rest_api.permissions import IsOwnerOrAdminOrReadOnly
@@ -6,12 +7,30 @@ from flixmix_rest_api.permissions import IsOwnerOrAdminOrReadOnly
 
 class ProfileList(generics.ListAPIView):
     # Only list profiles (creation is done with signals)
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        movie_count=Count('owner__movie', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'movie__count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetailView(generics.RetrieveUpdateAPIView):
     # Retrieve or update data if user is owner or admin
     permission_classes = [IsOwnerOrAdminOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        movie_count=Count('owner__movie', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
