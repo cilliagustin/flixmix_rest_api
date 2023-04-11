23528bb83967e1c8ac7f0e851e4b67a6d53a3ddd
Agustin cilli <cilli.agustin@gmail.com>
2023-04-11 07:17:42 +0000

    Deployment push

--- flixmix_rest_api/settings.py
+++ flixmix_rest_api/settings.py
@@ -137,16 +137,15 @@ WSGI_APPLICATION = 'flixmix_rest_api.wsgi.application'
 
 if 'DEV' in os.environ:
     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 else:
     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-    print('connected')
 
 
 # Password validation
