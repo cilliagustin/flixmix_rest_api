6ac5d41611774d84a3fae5bcec77e0ede86c2599
Agustin cilli <cilli.agustin@gmail.com>
2023-05-23 01:33:29 +0000

    Change list serializers

--- lists/serializers.py
+++ lists/serializers.py
@@ -8,7 +8,7 @@ class ListSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    movies = MovieSerializer(many=True, required=False)
+    # movies = MovieSerializer(many=True, read_only=True)
     comments_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
@@ -24,19 +24,19 @@ class ListSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['owner', 'created_at', 'updated_at']
 
-    def create(self, validated_data):
-        movie_data = validated_data.pop('movies')
-        instance = List.objects.create(**validated_data)
-        for movie in movie_data:
-            instance.movies.add(movie['id'])
-        return instance
+    # def create(self, validated_data):
+    #     movie_data = validated_data.pop('movies')
+    #     instance = List.objects.create(**validated_data)
+    #     for movie in movie_data:
+    #         instance.movies.add(movie['id'])
+    #     return instance
 
-    def update(self, instance, validated_data):
-        movie_data = validated_data.pop('movies', [])
-        if movie_data:
-            movie_serializer = MovieSerializer(many=True, data=movie_data)
-            movie_serializer.is_valid(raise_exception=True)
-            movie_objects = movie_serializer.save()
-            instance.movies.set(movie_objects)
-        instance = super().update(instance, validated_data)
-        return instance
+    # def update(self, instance, validated_data):
+    #     movie_data = validated_data.pop('movies', [])
+    #     if movie_data:
+    #         movie_serializer = MovieSerializer(many=True, data=movie_data)
+    #         movie_serializer.is_valid(raise_exception=True)
+    #         movie_objects = movie_serializer.save()
+    #         instance.movies.set(movie_objects)
+    #     instance = super().update(instance, validated_data)
+    #     return instance
