dab8268e763062d6546137d4e3a1a26202ed6cae
Agustin cilli <cilli.agustin@gmail.com>
2023-05-23 01:42:05 +0000

    Change list serializers

--- lists/serializers.py
+++ lists/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import List
-from movies.serializers import MovieSerializer
+from movies.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'poster']
 
 
 class ListSerializer(serializers.ModelSerializer):
@@ -8,7 +14,7 @@ class ListSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    # movies = MovieSerializer(many=True, read_only=True)
+    movies = MovieSerializer(many=True, read_only=True)
     comments_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
@@ -23,20 +29,3 @@ class ListSerializer(serializers.ModelSerializer):
             'movies', 'comments_count'
         ]
         read_only_fields = ['owner', 'created_at', 'updated_at']
-
-    # def create(self, validated_data):
-    #     movie_data = validated_data.pop('movies')
-    #     instance = List.objects.create(**validated_data)
-    #     for movie in movie_data:
-    #         instance.movies.add(movie['id'])
-    #     return instance
-
-    # def update(self, instance, validated_data):
-    #     movie_data = validated_data.pop('movies', [])
-    #     if movie_data:
-    #         movie_serializer = MovieSerializer(many=True, data=movie_data)
-    #         movie_serializer.is_valid(raise_exception=True)
-    #         movie_objects = movie_serializer.save()
-    #         instance.movies.set(movie_objects)
-    #     instance = super().update(instance, validated_data)
-    #     return instance
