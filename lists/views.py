9236c8bb0de1ca78a65d4711e73f640087bf52ea
Agustin cilli <cilli.agustin@gmail.com>
2023-05-24 08:13:16 +0000

    remove custom movie title search  from list

--- lists/views.py
+++ lists/views.py
@@ -24,7 +24,6 @@ class ListList(generics.ListCreateAPIView):
         'owner__profile',
     ]
     search_fields = [
-        'title',
         'movies__title',
     ]
     ordering_fields = [
@@ -38,13 +37,6 @@ class ListList(generics.ListCreateAPIView):
         title = self.request.query_params.get('title', None)
         if title is not None:
             queryset = queryset.filter(title__icontains=title)
-
-        # Filter by Movie title
-        movie_title = self.request.query_params.getlist('movie_title')
-        if movie_title:
-            queryset = queryset.filter(
-                movies__title__in=movie_title
-            ).distinct()
 
         # Filter by Owner Username
         owner = self.request.query_params.get('owner', None)
